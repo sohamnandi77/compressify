7d978ac786fe985f5485e7e7bd0f7ab0487dde11
Soham Nandi <sohamnandi77@gmail.com>
2025-01-26 16:45:40 +0530

    update image dimensions in video demo component

--- components/video-demo.tsx
+++ components/video-demo.tsx
@@ -4,8 +4,8 @@ import Image from "next/image";
 const VideoDemo = () => (
   <div className="px-6 relative">
     <Image
-      width={400}
-      height={700}
+      width={100}
+      height={405}
       src={"/demo3.gif"}
       className="h-full rounded-3xl w-full object-cover border-8 overflow-clip"
       alt="demo"
